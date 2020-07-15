2a234931bc5e2078f2d44737b621c625496c0934
Elad Karako <eladkarako@users.noreply.github.com>
2020-07-15 18:18:06 +0300

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+quick and dirty shellexecute in C#,
+max are 3 arguments all strings:
+first is file - doesn't have to be full path.
+second is single string of arguments.
+third is verb (open/print/printto/openas/runas/runasadmin/...)
+
+two arguments only apply file and arguments (verb is whatever it is in default)
+
+one argument only apply file.
+
+no arguments program ends.
+
+<br/><a href="https://paypal.me/e1adkarak0/5"><em>buy me a coffee ☕︎</em></a>  
+
---
+++ exe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace exe{
+  class Program{
+    public static void Main(string[] args){
+      Process process;
+      if(args.Length == 0) Environment.Exit(111);
+      
+      process = new Process();
+      process.StartInfo.UseShellExecute                 = true;
+      process.StartInfo.CreateNoWindow                  = false;
+      process.StartInfo.WindowStyle                     = ProcessWindowStyle.Maximized;
+      process.StartInfo.FileName                        = args[0];
+      if(args.Length > 1)  process.StartInfo.Arguments  = args[1];
+      if(args.Length > 2)  process.StartInfo.Verb       = args[2];
+      process.Start();
+
+      Environment.Exit(0);
+    }
+  }
+}
