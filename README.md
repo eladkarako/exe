33cf831856fbd15365de5cd344a518b824e0d6aa
Elad Karako <eladkarako@users.noreply.github.com>
2020-08-09 22:23:08 +0300

    allow more features such as working-directory, window-style, domain, username, password, ... - swallow all errors and exceptions plus run as window-application not console.

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
-quick and dirty shellexecute in C#,
-max are 3 arguments all strings:
-first is file - doesn't have to be full path.
-second is single string of arguments.
-third is verb (open/print/printto/openas/runas/runasadmin/...)
+A ShellExecuteW wrap that will never fail.
 
-two arguments only apply file and arguments (verb is whatever it is in default)
+<pre>
+0. filename            "calc.exe"
+1. arguments:          "hello world \"goodbye world\""  is 3 arguments: 'hello', 'world', 'goodbye world'
+2. working_directory    
+3. verb
+4. window_style        normal(default)/hidden/maximized/minimized
+5. is_load_profile:    false(default)/true
+6. domain:             
+7. username:           
+8. password:           
+</pre>
+  
+<a href="https://paypal.me/e1adkarak0/5"><em>be a pal!</em> buy me a cup of coffee ☕︎</a>  
+  
+no minimal requirements, the process will be trying to start anyway.  
+no console window, no error messages.
+just like <kbd>Start</kbd>-<kbd>Run</kbd>, no parent-process, and you can specify documents, batch-files, urls, resources that are in the PATH, network resources...
 
-one argument only apply file.
-
-no arguments program ends.
-
-<br/><a href="https://paypal.me/e1adkarak0/5"><em>buy me a coffee ☕︎</em></a>  
 
--- exe/Program.cs
+++ exe/Program.cs
@@ -1,22 +1,76 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Security;
 
 namespace exe{
   class Program{
-    public static void Main(string[] args){
-      Process process;
-      if(args.Length == 0) Environment.Exit(111);
-      
-      process = new Process();
-      process.StartInfo.UseShellExecute                 = true;
-      process.StartInfo.CreateNoWindow                  = false;
-      process.StartInfo.WindowStyle                     = ProcessWindowStyle.Maximized;
-      process.StartInfo.FileName                        = args[0];
-      if(args.Length > 1)  process.StartInfo.Arguments  = args[1];
-      if(args.Length > 2)  process.StartInfo.Verb       = args[2];
-      process.Start();
 
-      Environment.Exit(0);
+    public static String value_or_null(string[] array, uint index){ //normalize out-of-bound/empty to null. and value otherwize.
+      String value = null;
+      try {
+        value = array[index];
+      }catch (IndexOutOfRangeException){}
+      return value;
     }
+    
+    public static void Main(string[] args){
+      Process process = new Process();
+      
+      String filename          = value_or_null(args,0);
+      String arguments         = value_or_null(args,1);
+      String working_directory = value_or_null(args,2);
+      String verb              = value_or_null(args,3);
+      String window_style      = value_or_null(args,4);
+      String is_load_profile   = value_or_null(args,5);
+      String domain            = value_or_null(args,6);
+      String username          = value_or_null(args,7);
+      String password          = value_or_null(args,8);
+
+/*    //only useful for debugging when the application is in "console-mode" (see project properties- Project/PropertyGroup/OutputType: "Exe" to "WinExe" meaning no console mode).
+      Console.Error.WriteLine(String.Join("\r\n", new String[]{
+        "----=[RAW VALUES]=-------------------------------------"
+       ,"0. filename:           " + ">" + filename          + "<"
+       ,"1. arguments:          " + ">" + arguments         + "<"
+       ,"2. working_directory:  " + ">" + working_directory + "<"
+       ,"3. verb:               " + ">" + verb              + "<"
+       ,"4. window_style:       " + ">" + window_style      + "<"
+       ,"5. is_load_profile:    " + ">" + is_load_profile   + "<"
+       ,"6. domain:             " + ">" + domain            + "<"
+       ,"7. username:           " + ">" + username          + "<"
+       ,"8. password:           " + ">" + password          + "<"
+       ,"-------------------------------------------------------"
+      }));
+*/
+      process.StartInfo.UseShellExecute = true;
+      process.StartInfo.ErrorDialog = false;
+      process.StartInfo.ErrorDialogParentHandle = IntPtr.Zero;
+      process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;  //default.
+      process.StartInfo.LoadUserProfile = false;                  //default
+      
+      if(filename          != null)  process.StartInfo.FileName          = filename;
+      if(arguments         != null)  process.StartInfo.Arguments         = arguments;
+      if(working_directory != null)  process.StartInfo.WorkingDirectory  = working_directory;
+      if(verb              != null)  process.StartInfo.Verb              = verb;
+      if(window_style      != null){                              //possible override for default.
+             if(window_style.Equals("normal",   StringComparison.CurrentCultureIgnoreCase) == true)  process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+        else if(window_style.Equals("minimized",StringComparison.CurrentCultureIgnoreCase) == true)  process.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+        else if(window_style.Equals("maximized",StringComparison.CurrentCultureIgnoreCase) == true)  process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+        else if(window_style.Equals("hidden",   StringComparison.CurrentCultureIgnoreCase) == true)  process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+      }
+      if(is_load_profile   != null)  process.StartInfo.LoadUserProfile   = (is_load_profile.Equals("true",StringComparison.CurrentCultureIgnoreCase) == true);   //possible override for default.
+      if(domain            != null)  process.StartInfo.Domain            = domain;
+      if(username          != null)  process.StartInfo.UserName          = username;
+      if(password          != null){
+        SecureString secure_string = new SecureString();
+        char[] chars_password      = password.ToCharArray();
+        foreach (char c in chars_password) secure_string.AppendChar(c);
+        process.StartInfo.Password = secure_string;
+      }
+      
+      
+      try{
+        process.Start();
+      }catch(Exception){}
+   }
   }
 }
